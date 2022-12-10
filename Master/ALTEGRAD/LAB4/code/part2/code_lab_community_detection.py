b13f6967bf178821e783f4c0e368b61f9e9fc426
robachowyk <kaya.robach@gmail.com>
2022-12-10 17:37:49 +0100

    Update

---
+++ Master/ALTEGRAD/LAB4/code/part2/code_lab_community_detection.py
@@ -0,0 +1,48 @@
+"""
+Graph Mining - ALTEGRAD - Dec 2021
+"""
+
+import networkx as nx
+import numpy as np
+from scipy.sparse.linalg import eigs
+from random import randint
+from sklearn.cluster import KMeans
+
+
+############## Task 6
+# Perform spectral clustering to partition graph G into k clusters
+def spectral_clustering(G, k):
+    
+    ##################
+    # your code here #
+    ##################
+    
+    return clustering
+
+
+
+############## Task 7
+
+##################
+# your code here #
+##################
+
+
+
+############## Task 8
+# Compute modularity value from graph G based on clustering
+def modularity(G, clustering):
+    
+    ##################
+    # your code here #
+    ##################
+    
+    return modularity
+
+
+
+############## Task 9
+
+##################
+# your code here #
+##################
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Education
- Projects done during my studies.
+
+ Projects, homeworks, practical works, done during my studies.
+
+ - Double bachelor degree in Economics and Mathematics at [TSE](https://www.tse-fr.eu/fr/licence-1-licence-2-economie-et-mathematiques)
+ - Magistere degree in Statistics for Economics at [TSE](https://www.tse-fr.eu/fr/magistere-deconomiste-statisticien)
+ - Master 1 in Applied Mathematics: Econometrics, Statistics, Game theory at [TSE](https://www.tse-fr.eu/master-mathematics-and-economic-decision?qt-embed_generic_tabs=1#qt-embed_generic_tabs)
+ - Master 2 in Mathematics, Vision, Learning at [ENS Paris Saclay](https://www.master-mva.com/) 
